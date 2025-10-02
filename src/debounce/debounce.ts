3b6ba4933b4a79a976da1c25698ae0168a264f8c
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-02 14:50:45 +0330

    Update `debounce` function to support variable arguments and improve type definitions

--- src/debounce/debounce.ts
+++ src/debounce/debounce.ts
@@ -1,3 +1,11 @@
-export function debounce(fn: Function, delay: number) {
-
+export function debounce(fn: (...args: any[]) => void, delay: number) {
+  let timeout: NodeJS.Timeout | null = null
+  return(...args: any[]) => {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => {
+      fn(...args)
+    }, delay)
+  }
 }
