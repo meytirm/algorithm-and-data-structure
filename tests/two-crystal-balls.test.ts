b45fee8163d3b165781a53621c911e84d559c6cd
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-03 10:59:02 +0330

    Expand tests for `twoCrystalBalls` to cover edge cases

--- tests/two-crystal-balls.test.ts
+++ tests/two-crystal-balls.test.ts
@@ -5,6 +5,20 @@ describe('Two Crystal Balls Algorithm', () =>{
     const arr = [false, false, false, false, false, false, false, false, false, true, true, true, true]
     const crystalBall = twoCrystalBalls(arr)
 
-    expect(crystalBall).toBe(8)
+    expect(crystalBall).toBe(9)
+  });
+
+  it('should return -1 if no true value', () => {
+    const arr = [false, false, false, false, false, false, false, false, false]
+    const crystalBall = twoCrystalBalls(arr)
+
+    expect(crystalBall).toBe(-1)
+  });
+
+  it('should return 0 if first value is true', () => {
+    const arr = [true, true, true, true, true, true, true, true, true]
+    const crystalBall = twoCrystalBalls(arr)
+
+    expect(crystalBall).toBe(0)
   });
 })
