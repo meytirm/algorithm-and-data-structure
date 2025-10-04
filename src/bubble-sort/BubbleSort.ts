b8e8aec439978b5d7606d2556d1b1f2461719c73
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-04 14:41:01 +0330

    Add return type annotation to bubbleSort function

--- src/bubble-sort/BubbleSort.ts
+++ src/bubble-sort/BubbleSort.ts
@@ -1,4 +1,4 @@
-export function bubbleSort(arr: Array<number>) {
+export function bubbleSort(arr: Array<number>): Array<number> {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) {
