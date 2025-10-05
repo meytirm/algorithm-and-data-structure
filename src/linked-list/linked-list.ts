ceedee759b370df111902fb623588641e30289db
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-06 00:19:05 +0330

    Refactor LinkedList implementation to use generics and define Node and LinkedList interfaces

--- src/linked-list/linked-list.ts
+++ src/linked-list/linked-list.ts
@@ -1,14 +1,90 @@
-interface SNode {
-  value: number;
-  next: SNode | null;
-  prev: SNode | null;
+interface NodeInterface<T> {
+  value: T;
+  next: NodeInterface<T> | null;
+  prev: NodeInterface<T> | null;
 }
 
-interface LinkedList {
-  head: SNode | null;
-  tail: SNode | null;
+interface LinkedListInterface<T> {
+  head: NodeInterface<T> | null;
+  tail: NodeInterface<T> | null;
 
-  get(index: number): SNode | null;
-  add(value: number): void;
-  remove(index: number): void;
+  isEmpty(): boolean;
+
+  get(value: T): NodeInterface<T> | null;
+
+  append(value: T): void;
+
+  prepend(value: T): void
+
+  remove(value: T): void;
+}
+
+class SingleNode<T> implements NodeInterface<T> {
+  value: T;
+  next: SingleNode<T> | null;
+  prev: SingleNode<T> | null;
+
+  constructor(value: T) {
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+export class LinkedList<T> implements LinkedListInterface<T> {
+  head: SingleNode<T> | null;
+  tail: SingleNode<T> | null;
+
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  isEmpty() {
+    return this.head === null && this.tail === null;
+  }
+
+  append(value: T) {
+    const newNode = new SingleNode(value);
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    const currentTail = this.tail;
+    if (currentTail) {
+      currentTail.next = newNode
+      this.tail = newNode;
+    }
+  }
+
+  prepend(value: T) {
+    const newNode = new SingleNode(value);
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    const currentHead = this.head;
+    if (currentHead) {
+      currentHead.prev = newNode;
+      this.head = newNode;
+    }
+  }
+
+  get(value: T): SingleNode<T> | null {
+    if (!this.isEmpty()) {
+      let current = this.head
+        while (current) {
+          if (current.value === value) {
+            return current;
+          }
+          current = current.next;
+        }
+    }
+    return null;
+  }
+
+  remove(value: T) {
+  }
 }
