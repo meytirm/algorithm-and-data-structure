deb1277ef74abfbc90dee340d22a9ff853e7c061
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-06 00:19:14 +0330

    Add additional test cases for LinkedList creation, append, and retrieval

--- tests/linked-list.test.ts
+++ tests/linked-list.test.ts
@@ -1,5 +1,15 @@
+import {LinkedList} from "../src/linked-list/linked-list";
+
 describe('linked list', () => {
   it('should create a linked list', () => {
+    const linkedList = new LinkedList();
+    expect(linkedList.isEmpty()).toBe(true);
+    linkedList.append(1);
+    expect(linkedList.isEmpty()).toBe(false);
+    const node = linkedList.get(1)
 
+    if (node) {
+      expect(node.value).toBe(1);
+    }
   })
 });
