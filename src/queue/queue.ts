c246ddc1874ef81703585dae2f2fd8f5d0d30d6c
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-08 11:24:39 +0330

    Implement initial Queue class and refine interfaces for nodes and queue

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -1,14 +1,29 @@
-interface QueueNode<T> {
+interface QueueNodeInterface<T> {
   value: T;
-  next: QueueNode<T>
+  next: QueueNodeInterface<T>;
 }
 
 interface QueueInterface<T> {
-  head: QueueNode<T> | null;
-  tail: QueueNode<T> | null;
+  head: QueueNodeInterface<T> | undefined;
+  tail: QueueNodeInterface<T> | undefined;
 
   enqueue(value: T): void;
   dequeue(): T | undefined;
   peek(): T | undefined;
 }
 
+class Queue<T> implements QueueInterface<T> {
+  head: QueueNodeInterface<T> | undefined;
+  tail: QueueNodeInterface<T> | undefined;
+  constructor() {
+    this.head = undefined;
+    this.tail = undefined;
+  }
+
+  enqueue(value: T) {
+  }
+  dequeue() {
+  }
+  peek() {
+  }
+}
