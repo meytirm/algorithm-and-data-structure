c19a5886028562aa25de33978ae05a736b2330c2
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-27 11:21:38 +0330

    Initialize `maze-solver` module with `Point` type and function stubs

---
+++ src/maze-solver/maze-solver.ts
@@ -0,0 +1,13 @@
+export type Point = {
+  x: number;
+  y: number;
+}
+
+function walk(maze: string[], wall: string, curr: Point, end: Point):boolean {
+
+
+}
+
+export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+
+}
