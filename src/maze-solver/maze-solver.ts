e2f55fefd6d3fc245f43b3ef0e24e0a2c1548a8d
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-27 11:23:00 +0330

    Add wall check in `walk` function to prevent invalid maze traversal

--- src/maze-solver/maze-solver.ts
+++ src/maze-solver/maze-solver.ts
@@ -7,6 +7,10 @@ function walk(maze: string[], wall: string, curr: Point, end: Point):boolean {
   if (curr.x < 0 || curr.y > maze[0].length || curr.y < 0 || curr.y > maze.length) {
     return false;
   }
+
+  if (maze[curr.y][curr.x] === wall) {
+    return false;
+  }
 }
 
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
