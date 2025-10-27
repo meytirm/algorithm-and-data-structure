30579d0957d0ecd24f8e611510713333bdc673dc
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-27 11:21:53 +0330

    Add boundary check for `walk` function in `maze-solver` module

--- src/maze-solver/maze-solver.ts
+++ src/maze-solver/maze-solver.ts
@@ -4,8 +4,9 @@ export type Point = {
 }
 
 function walk(maze: string[], wall: string, curr: Point, end: Point):boolean {
-
-
+  if (curr.x < 0 || curr.y > maze[0].length || curr.y < 0 || curr.y > maze.length) {
+    return false;
+  }
 }
 
 export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
