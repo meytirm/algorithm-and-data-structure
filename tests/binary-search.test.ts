08df61ea479be2827c971acc041c2c1aac7c6c87
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-10-28 11:22:41 +0330

    Add recursive binary search test case to binary search tests

--- tests/binary-search.test.ts
+++ tests/binary-search.test.ts
@@ -1,5 +1,7 @@
 import binarySearch from "../src/binary-search/binary-search";
+import binarySearchRecursion from "../src/binary-search/binary-search-recursion";
 
 test("binarySearch finds existing element", () => {
   expect(binarySearch([1, 3, 5, 7], 1)).toBe(0);
+  expect(binarySearchRecursion([1, 3, 5, 7], 1)).toBe(0);
 });
