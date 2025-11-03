dfd12cf0f52cd7da7c60651fa270509fcc8d4791
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-03 12:52:40 +0330

    Complete in-place Quick Sort implementation

--- src/quick-sort/quick-sort-in-place.ts
+++ src/quick-sort/quick-sort-in-place.ts
@@ -2,23 +2,30 @@ export default function quickSortInPlace (arr: number[]) {
   quickSort(arr, 0, arr.length - 1)
 }
 
-function quickSort(arr: number[], low: number, high: number) {}
+function quickSort(arr: number[], low: number, high: number) {
+  if (low >= high) return
+
+  const pivotIndex = partition(arr, low, high)
+
+  quickSort(arr, low, pivotIndex - 1)
+  quickSort(arr, pivotIndex + 1, high)
+}
 function partition(arr: number[], low: number, high: number) {
   const pivot = arr[high]
 
-  let index = low - 1
+  let i = low - 1
 
-  for (let i = low; i < high; i++) {
-    if (arr[i] <= pivot) {
-      index++
-      const temp = arr[i]
-      arr[i] = arr[index]
-      arr[index] = temp
+  for (let j = low; j < high; j++) {
+    if (arr[j] <= pivot) {
+      i++
+      const temp = arr[j]
+      arr[j] = arr[i]
+      arr[i] = temp
     }
   }
 
-  index++
-  arr[high] = arr[index]
-  arr[index] = pivot
-  return index
+  i++
+  arr[high] = arr[i]
+  arr[i] = pivot
+  return i
 }
